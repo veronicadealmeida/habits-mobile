deb679373146254482e43fd7da599fb657d5dc9b
Veronica <almeida.vrc@gmail.com>
2023-06-06 09:32:03 -0300

    Cria component Loading

--- habits-mobile/App.tsx
+++ habits-mobile/App.tsx
@@ -1,7 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
+
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
@@ -20,6 +40,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    color: 'blue',
+    color: '#FFF',
+    fontFamily: 'Inter_800ExtraBold',
   },
 });
